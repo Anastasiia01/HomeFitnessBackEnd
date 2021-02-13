65f1a8eaf638b6d03ca0a2621dd9a545621a056a
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-02-13 14:36:24 -0500

    Defined format of the input JSON object for the routes.

--- app/__init__.py
+++ app/__init__.py
@@ -6,16 +6,33 @@ from .datalayer import DataLayer
 app = Flask(__name__)
 app.config.from_object('app.config.Config')
 dataLayer = DataLayer(app.config['GOOGLE_API_KEY'])
-print(dataLayer)
-
-
-
 
 if __name__ == "main":
     app.run()
 
-@app.route('/', methods=['POST'])
-def index():
+@app.route('/channel', methods=['POST'])
+def get_channel_id():
+    """
+    Receives query with json obj that contains a single  key:
+        channel_name: str
+    Returns channel ID.
+    """
+
+@app.route('/video', methods=['POST'])
+def get_video():
+    """
+    Receives query with the json object and returns link to the most suitable video.
+    Format of input JSON obj:
+        channel_id: str 
+        workout_type: str[] (e.g. [body_part, cardio])
+        mins: int  (then used for videoDuration: "long"|"medium")  
+    """
+
     request_data = request.get_json() #get parameters from request
-    channel_id = request_data['channel_id']
+
+    channel_id = None
+
+    if request_data:
+        if 'channel_id' in request_data:
+            channel_id = request_data['channel_id']
     return "The Youtube channel ID is: {}".format(channel_id)
--- app/datalayer.py
+++ app/datalayer.py
@@ -7,8 +7,24 @@ class DataLayer:
     def __init__(self, key):
         self.api_key = key
     
-    def __str__(self):
-        return "key: " + self.api_key
+    """def __str__(self): #for debugging purposes
+        return "key: " + self.api_key"""
+
+    def video_search(self):
+        """request:{
+            channelId:string,
+            q:string,
+            maxResults:number,
+            relevanceLanguage: string ("en"),
+            publishedAfter:datetime ("RFC 3339 formatted date-time value (1970-01-01T00:00:00Z)"),
+            type: "video"|"channel"|,
+            videoDefinition: "any"|"standard"|"high",
+            videoDimension: "2d",
+            videoDuration: "long"|"medium",
+            videoEmbeddable: "true",
+        }"""
+        return None
+
 
     
     
