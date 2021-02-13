9a4b2e4902068ab521dce70583485412e11991e4
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-02-13 10:39:15 -0500

    Added DataLayer class that will communicate with Youtube Data API.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session
+from .datalayer import DataLayer
 #import requests
 
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
+dataLayer = DataLayer(app.config['GOOGLE_API_KEY'])
+
 
 
 
--- app/datalayer.py
+++ app/datalayer.py
@@ -1,4 +1,9 @@
+"""Datalayer that will allow our server to utilize Youtube Data API"""
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 
+class DataLayer:
+    def __init__(self, key):
+        self.api_key = key
+    
