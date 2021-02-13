4764d41913e5d6b70b1f2b196a1457c9b79cbce7
Anastasiia Babenko <ababenko@my.bridgeport.edu>
2021-02-13 10:27:46 -0500

    Updated config file to store Google API key.

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect, url_for, session
 
 
 app = Flask(__name__)
-#app.config.from_pyfile('config.py')
 app.config.from_object('config.Config')
 
 
--- app/config.py
+++ app/config.py
@@ -3,10 +3,11 @@ from os import environ, path
 from dotenv import load_dotenv
 
 basedir = path.abspath(path.dirname(__file__))
-print(basedir)
-#load_dotenv(path.join(basedir, '.env'))
+load_dotenv(path.join(basedir, '.env'))
 
 class Config:
     #Sets Flask config variables.
     FLASK_ENV = 'development'
+    GOOGLE_API_KEY = environ.get('GOOGLE_API_KEY') #request this API key through your own Google API Console
+
     
